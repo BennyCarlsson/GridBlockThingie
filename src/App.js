c08f25976dcac9d0bc2d986848dea58d4666a80e
Benny <carlsson.benny93@gmail.com>
2017-08-16 06:39:34 +0200

    started on grid

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    let gridBlocks = [];
+    for (var i = 0; i < 10; i++) {gridBlocks.push(<GridBlock key={i} index={i} />)}
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        {gridBlocks}
       </div>
-    );
+    )
+  }
+}
+
+class GridBlock extends Component{
+  onClick(e){
+    console.log(this.props.index)
+  }
+  render(){
+    return(
+      <div className={"gridBlock item"+this.props.index} onClick={this.onClick.bind(this)}></div>
+    )
   }
 }
 
