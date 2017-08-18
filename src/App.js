c02e704b60bcfd7361c1e66952c4cbede9869840
Benny <carlsson.benny93@gmail.com>
2017-08-18 06:28:59 +0200

    loops out gridblocks

--- src/App.js
+++ src/App.js
@@ -2,24 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.grids = [["1/3","1/3"],["4/4","1/3"],["1/2","3/5"]];
+  }
   render() {
-    let gridBlocks = [];
-    for (var i = 0; i < 10; i++) {gridBlocks.push(<GridBlock key={i} index={i} />)}
+    let standardGridBlocks = [];
+    for (var i = 0; i < 100; i++) {standardGridBlocks.push(<StandardGridBlock key={i} index={i} />)}
+    let createdGrids = [];
+    this.grids.map((val,i) => createdGrids.push(<CreatedGridBlock key={i} col={val[0]} row={val[1]}/>))
     return (
       <div className="container">
-        {gridBlocks}
+        {createdGrids}
+        {standardGridBlocks}
       </div>
     )
   }
 }
-
-class GridBlock extends Component{
+class CreatedGridBlock extends Component{
+  onClick(e){
+    console.log("big")
+  }
+  render(){
+    return(
+      <div className={"gridBlock item"+this.props.index} 
+        style={{gridColumn:this.props.col,gridRow:this.props.row}} onClick={this.onClick.bind(this)}></div>
+    )
+  }
+}
+class StandardGridBlock extends Component{
   onClick(e){
     console.log(this.props.index)
   }
   render(){
     return(
-      <div className={"gridBlock item"+this.props.index} onClick={this.onClick.bind(this)}></div>
+      <div className={"gridBlock standardGridBlock item"+this.props.index} onClick={this.onClick.bind(this)}></div>
     )
   }
 }
