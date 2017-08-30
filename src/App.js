bf55777cb6d81cbec2d789a0eb07bc0a8a5f836b
Benny <carlsson.benny93@gmail.com>
2017-08-30 05:52:17 +0200

    now possible to add boxes in any order

--- src/App.js
+++ src/App.js
@@ -16,14 +16,14 @@ class App extends Component {
   }
   removeFromGrid(matrix){
     console.log("row1 "+matrix[0][0][0][0])
-    let row1 = matrix[0][0][0][0]
+    let row1 = matrix[0][0][0][0] <= matrix[1][0][0][0] ? matrix[0][0][0][0] : matrix[1][0][0][0] 
     console.log("row2 "+matrix[0][0][1][0])
-    let row2 = matrix[0][0][1][0]
+    let row2 = matrix[0][0][1][0] <= matrix[1][0][1][0] ? matrix[0][0][1][0] : matrix[1][0][1][0]
 
     console.log("col1 "+matrix[1][0][0][0])
-    let col1 = matrix[1][0][0][0]
+    let col1 = matrix[1][0][0][0] > matrix[0][0][0][0] ? matrix[1][0][0][0] : matrix[0][0][0][0]
     console.log("col2 "+matrix[1][0][1][0])
-    let col2 = matrix[1][0][1][0]
+    let col2 = matrix[1][0][1][0] > matrix[0][0][1][0] ? matrix[1][0][1][0] : matrix[0][0][1][0]
 
     let array = this.state.standardGridBlocks.slice()
     for(let i = row1;i <= col1;i++){
